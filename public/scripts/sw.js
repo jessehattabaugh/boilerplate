4e77c6c84158a4be5723ef2a562f47b9097a7ce7
Jesse Hattabaugh <jessehattabaugh@protonmail.com>
2024-07-06 19:03:50 -0700

    removing eslint errors

--- public/scripts/sw.js
+++ public/scripts/sw.js
@@ -1,5 +1,9 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/6.2.0/workbox-sw.js');
+// eslint-disable-next-line no-undef
 const { registerRoute } = workbox.routing;
+// eslint-disable-next-line no-undef
 const { CacheFirst } = workbox.strategies;
-registerRoute(() => true, new CacheFirst());
-console.debug('👋👷‍♂️ service worker caching enabled');
+registerRoute(() => {
+	return true;
+}, new CacheFirst());
+console.debug('👋👷‍♂️ service worker cacheing enabled');
